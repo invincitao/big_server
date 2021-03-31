9b18c89ebb3571b50c3723ffd893a71622cd12b4
dxm <dxm@123.cn>
2021-03-31 10:20:47 +0800

    2-静态托管

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const server = express();
 // 跨域
 const cors = require('cors');
 server.use(cors());
-
+// 设为静态托管
+server.use('/uploads', express.static('uploads'));
 server.listen(3000, () => {
     console.log('3000端口准备就绪');
 })
